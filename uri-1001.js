d7caddce98ac22124cb3e670a7ad9527dc40daa9
Sara Cavalcante <designersaracavalcante@gmail.com>
2021-10-16 10:16:22 -0300

    Responde o problema 1001

---
+++ uri-1001.js
@@ -0,0 +1,10 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+
+var lines = input.split('\n');
+
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+var X = A + B;
+
+console.log('X = ' + X);
+
